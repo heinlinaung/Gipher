007f822bd7eda11647b93001e2d23544cb85ea11
Hein Lin Aung <heinlinaung@amdon.com>
2018-03-21 10:29:15 +0630

    Added 404 image on err connections

--- fetch-random.js
+++ fetch-random.js
@@ -1,16 +1,13 @@
+'use strict';
+let source_img = document.getElementsByTagName('img')[0]
+
 fetch('https://api.giphy.com/v1/gifs/random?api_key=RzhJZxufesXDEQ5feRw92UZruYYA8wob&tag=emma-watson&rating=G')
   .then(function(response) {
     return response.json();
-    // console.log('response: ',response)
-    // let source_img = document.getElementsByTagName('img')[0]
-    // source_img.src =response.images.original.webp;
-    // return response;
   })
   .then(function(myJson) {
-    let source_img = document.getElementsByTagName('img')[0]
-    console.log('myJson: ',myJson)
     source_img.src = myJson.data.images.original.webp;
   })
   .catch(err=>{
-    console.log('err: ',err)
+    source_img.src = '404.gif'
   })
